99685e559e4a2004110304406d351a1b35ae56b0
Rafał Kowalczyk <raff1410X@gmail.com>
2023-01-05 16:49:18 +0100

    initial

--- src/views/mandala/menu/Logo.tsx
+++ src/views/mandala/menu/Logo.tsx
@@ -3,16 +3,20 @@
 function Logo() {
 
     return (
-        <div className="logo">
-            <div className="logo-letters">
-                <span>Homo<br/>affect</span>
-                <span className="logo-red">us&nbsp;&nbsp;&nbsp;</span>
-            </div>
-            <div className="logo-letters logo-small">
-                <span>mandala creators</span>
-            </div>
-        </div>
-    );
+        <img className="logo-img" alt="" src={require("../../../assets/logo.png")}/>
+    )
+
+    // return (
+    //     <div className="logo">
+    //         <div className="logo-letters">
+    //             <span>Homo<br/>affect</span>
+    //             <span className="logo-red">us&nbsp;&nbsp;&nbsp;</span>
+    //         </div>
+    //         <div className="logo-letters logo-small">
+    //             <span>mandala creators</span>
+    //         </div>
+    //     </div>
+    // );
 }
 
 export default Logo;
