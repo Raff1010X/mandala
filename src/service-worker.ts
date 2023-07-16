45e24ac8f31f8476d9bbaadf566654e42e604934
Rafał Kowalczyk <raff1410X@gmail.com>
2023-07-16 21:47:14 +0200

    add: cache for api/mandala get requests

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -23,46 +23,78 @@ registerRoute(
 );
 
 // save to indexedDB for '/api/mandala'
-async function saveMandalaToIndexedDB(data: any) {
+async function saveMandalaToIndexedDB(databaseName: string, data: any, storeName = 'mandala-store') {
   if ('SyncManager' in self) {
     const object = await data.json()
-    const db = await openDB('mandala', 1, 'mandala-store')
-    saveObject(db, object, 'mandala-store')
-    .then(() => {
-      console.log('Mandala saved to indexedDB')
-    })
-    .catch((err: any) => {
-      console.log('Mandala save to indexedDB failed: ',err )
-    })
+    const db = await openDB(databaseName, 1, storeName)
+    saveObject(db, object, storeName)
+      .then(() => {
+        console.log('Mandala saved to indexedDB')
+      })
+      .catch((err: any) => {
+        console.log('Mandala save to indexedDB failed: ', err)
+      })
   }
 }
 
-// network first then save to indexedDB for '/api/mandala'
+
 self.addEventListener('fetch', (event: any) => {
+
+  // network first then save to indexedDB for '/api/mandala'
   if (event.request.method === 'POST') {
     if (event.request.url.includes('/api/mandala')) {
       event.respondWith(
         fetch(event.request.clone())
           .then((response: any) => {
             if (response.status !== 201) {
-              saveMandalaToIndexedDB(event.request);
+              saveMandalaToIndexedDB('background-sync', event.request);
               return new Response(JSON.stringify({ status: 'saved' }));
             } else {
               return response;
             }
           })
           .catch((err: any) => {
-            saveMandalaToIndexedDB(event.request);
+            saveMandalaToIndexedDB('background-sync', event.request);
             return new Response(JSON.stringify({ status: 'saved' }));
           })
       );
     }
   }
-});
+
+  // cache first (form indexedDB ) then network (and save to indexedDB) for '/api/mandala' get request
+  if (event.request.method === 'GET') {
+    if (event.request.url.includes('/api/mandala')) {
+      event.respondWith(
+        (async () => {
+          const db: IDBDatabase | any = await openDB('gallery', 1, 'mandala-store');
+          const mandala = await getAll(db, 'mandala-store');
+          let id = Number(event.request.url.split('/').pop());
+          if (id === -1 || id > mandala.length) id = 1;
+          const item = mandala.filter((item: any) => item.value.id === id);
+          if (item.length > 0) {
+            return new Response(JSON.stringify(item[0].value));
+          } else {
+            return fetch(event.request.clone())
+              .then((response: any) => {
+                if (response.status !== 200) {
+                  return new Response(JSON.stringify({ status: 'failed' }));
+                }
+                saveMandalaToIndexedDB('gallery', response.clone(), 'mandala-store');
+                return response;
+              })
+              .catch((err: any) => {
+                return new Response(JSON.stringify({ status: 'failed' }));
+              });
+          }
+        })()
+      );
+    }
+  }
+})
 
 // background sync for '/api/mandala'
 async function syncMandala() {
-  const db: IDBDatabase | any = await openDB('mandala', 1, 'mandala-store');
+  const db: IDBDatabase | any = await openDB('background-sync', 1, 'mandala-store');
   const mandala = await getAll(db, 'mandala-store');
   mandala.forEach(async (item: any) => {
     const response = await API.makePost('/api/mandala', item.value);
